fae505850fa1052af0a90b7b391ca4d12ccd3d9a
Luzifer <mrcookiesmedia@gmail.com>
2020-01-24 15:47:08 +0100

    Update 0.2.2

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,7 @@
 require "config"
+
 local m = "__Senpais_Pipes__"
+
 data:extend
 {
 	{
@@ -10,6 +12,7 @@ data:extend
 		order = "gg"
 	}
 }
+
 local sn = 0
 local pe, pd, ps, pdv = {}, {}, {}, {}
 local c =
@@ -39,16 +42,21 @@ local void_icon =
 	width = 32,
 	height = 32
 }
+
 for _, a in pairs { "N", "E", "S", "W" } do
-	pd[a] = { icon = m .. "/graphics/pipe_indication_" .. a .. ".png" }
-	ps[a] = { icon = m .. "/graphics/pipe_indication_shadow_" .. a .. ".png" }
-	pdv[a] = { icon = m .. "/graphics/pipe_indication_" .. a .. ".png", tint = { r = 0.6, g = 0, b = 0.6 } }
+	pd[a] = { icon = m .. "/graphics/pipe_indication_" .. a .. ".png", icon_size = 32 }
+	ps[a] = { icon = m .. "/graphics/pipe_indication_shadow_" .. a .. ".png", icon_size = 32 }
+	pdv[a] = { icon = m .. "/graphics/pipe_indication_" .. a .. ".png", icon_size = 32, tint = { r = 0.6, g = 0, b = 0.6 } }
 end
+
 for _, o in pairs( data.raw["pipe"] ) do
 	if not Senpais.Pipes.NOCOPYS[o.name] then
 		local on = 0
+		
 		sn = sn + 1
+		
 		if sn < 10 then sp = "0" else sp = "" end
+		
 		data:extend
 		{
 			{
@@ -58,22 +66,36 @@ for _, o in pairs( data.raw["pipe"] ) do
 				order = sp .. sn
 			}
 		}
+		
 		for s, b in pairs( Senpais.Pipes.Directions ) do
 			on = on + 1
+			
 			if on < 10 then op = "0" else op = "" end
+			
 			local np = util.table.deepcopy( data.raw["pipe"][o.name] )
 			np.name = o.name .. "-" .. s
 			np.icon = nil
-			np.icons = { { icon = o.icon } }
+			np.icon_size = nil
+			np.icon_mipmap = nil
+			np.icons = { { icon = o.icon, icon_size = o.icon_size or nil, icon_mipmap = o.icon_mipmap or nil } }
 			np.subgroup = "pipe-subgroup-" .. sp .. sn
 			np.order = "pipe-" .. op .. on
 			np.fluid_box = { base_area = o.fluid_box.base_area, pipe_connections = b.connec }
 			np.localised_name = { "entity-name.Senpais-Pipes", {"entity-name." .. o.name }, { "Senpais-Pipes." .. s } }
 			np.placeable_by = { item = o.name, count = o.minable and o.minable.count or 1 }
+
+			--[[if o.icon_size then
+				np.icons[1].icon_size = o.icon_size
+			end
+
+			if o.icon_mipmap then
+				np.icons[1].icon_mipmap = o.icon_mipmap
+			end]]
+
 			if s == "void" then
 				np.type = "infinity-pipe"
 				np.gui_mode = "none"
-				table.insert( np.icons, { icon = m .. "/graphics/pipe_indication_void.png" } )
+				table.insert( np.icons, { icon = m .. "/graphics/pipe_indication_void.png", icon_size = 32 } )
 				for _, name in pairs( c ) do
 					np.pictures[name] = { layers = { np.pictures[name], void_icon } }
 				end
